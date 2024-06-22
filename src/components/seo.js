a13b577f9e8b89a27fe8f1ce836fb244baebaf22
Andrew Brown <andrew.brown@tempest.com>
2024-06-21 20:57:33 -0400

    remove Seo.DefaultProps and error that came with it

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const Seo = ({ description, lang, meta, title }) => {
+const Seo = ({ description = '', lang = 'en', meta = [], title }) => {
   const { wp, wpUser } = useStaticQuery(
     graphql`
       query {
@@ -75,12 +75,6 @@ const Seo = ({ description, lang, meta, title }) => {
       ].concat(meta)}
     />
   )
-}
-
-Seo.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
 }
 
 Seo.propTypes = {
